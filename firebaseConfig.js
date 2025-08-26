1d17ccd14610bc85f5821bb54ceabbc207adbd22
rflixtop <sprlivetv@gmail.com>
2025-08-26 07:12:58 +0600

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyBVCQyLO6GG0ylIzYdIij4GfulHWchnVp4",
+  authDomain: "livetv-5a773.firebaseapp.com",
+  databaseURL: "https://livetv-5a773-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "livetv-5a773",
+  storageBucket: "livetv-5a773.appspot.com",
+  messagingSenderId: "814020931617",
+  appId: "1:814020931617:web:710213b3212fb039f67360",
+  measurementId: "G-9YBS0CLR63"
+};
